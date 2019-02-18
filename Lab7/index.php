2f78d9dfb3132c6d9b5bff5d8a935719a15ee26d
Nwakuso <1808891@rgu.ac.uk>
2019-02-18 12:15:52 +0000

    php labs

--- Lab7/index.php
+++ Lab7/index.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1808891
- * Date: 18/02/2019
- * Time: 11:16
- */
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+    echo "Hello," . " ". "world" . "!";
+    echo 5 * 7;
+    $myname = "Frodo Baggins";
+    $myage = 111;
+    echo "My name is" . $myname . "and i am" . $myage;
+    echo "I get printed!"
+
+?>
+    <?php
+      $myname ="Edgar";
+
+      if ( $myname == "Simon") {
+        print  "I know you!";
+      }
+      else{
+         print "who are you?";
+      }
+      ?>
+
+    <?php
+         $numberofHobbits = 2;
+
+         switch ($numberOfhobbits) {
+        case 1:
+          echo "1 sad hobbit";
+          break;
+        case 2:
+          echo "2 happy hobbits";
+          break;
+        case 3:
+          echo "3 hobbits are a crowd";
+          break;
+        default:
+          echo "All the hobbits have gone home";
+        }
+        ?>
+</p>
+</body>
+</html>
